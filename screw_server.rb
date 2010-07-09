3b5a0f9b8adb797d35520c46cd5ee355356f7b26
Kris Hanekamp <kristian.hanekamp@infopark.de>
2010-07-09 16:07:38 +0200

    [screw server] refactored: specs and fixtures are classes now

--- screw_server.rb
+++ screw_server.rb
@@ -26,10 +26,6 @@ def scan_for_statement(statement, filename)
   File.read(filename).scan(/#{statement}\(["'](.+)['"]\)/).map { |groups| groups[0] }
 end
 
-def fixtures_used_in(filename)
-  scan_for_statement("use_fixture", filename)
-end
-
 def required_files_in(filename)
   files = scan_for_statement("require", filename)
   files = files.reject { |file| file == "spec_helper.js" }
@@ -37,9 +33,9 @@ def required_files_in(filename)
 end
 
 def required_files_for(specs)
-  requires = required_files_in($spec_base_path + "spec_helper.js")
+  requires = []
   specs.each do |spec|
-    requires += required_files_in($spec_base_path + spec + "_spec.js")
+    requires += spec.required_scripts
   end
   requires.uniq
 end
@@ -47,17 +43,52 @@ end
 def fixture_hash_for(specs)
   fixture_html = {}
   specs.each do |spec|
-    fixtures_used_in($spec_base_path + spec + "_spec.js").each do |fixture|
-      fixture_file = "#{$base_dir}/spec/javascripts/fixtures/#{fixture}.html"
-      fixture_html[fixture] ||= File.read(fixture_file).match(/<body>(.+)<\/body>/m)[1]
+    spec.used_fixtures.each do |fixture|
+      fixture_html[fixture.name] ||= File.read(fixture.filename).match(/<body>(.+)<\/body>/m)[1]
     end
   end
   fixture_html
 end
 
+class FixtureFile
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def filename
+    "#{$spec_base_path}/fixtures/#{name}.html"
+  end
+end
+
+class SpecFile
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def filename
+    $spec_base_path + name + "_spec.js"
+  end
+
+  def url
+    $spec_path + name + "_spec.js"
+  end
+
+  def used_fixtures
+    scan_for_statement("use_fixture", filename).map {|name| FixtureFile.new(name) }
+  end
+
+  def required_scripts
+    required_files_in($spec_base_path + "spec_helper.js") + required_files_in(filename)
+  end
+end
+
 def all_specs
   Dir.glob($spec_base_path + "*_spec.js").map do |file|
-    file.gsub($spec_base_path, "").gsub("_spec.js", "")
+    SpecFile.new(file.gsub($spec_base_path, "").gsub("_spec.js", ""))
   end
 end
 
@@ -67,7 +98,7 @@ class ScrewServer < Sinatra::Base
   set :views, File.dirname(__FILE__) + '/views'
 
   get "/run/:name" do
-    run_specs([params[:name]])
+    run_specs([SpecFile.new(params[:name])])
   end
 
   get "/run" do
@@ -82,7 +113,8 @@ class ScrewServer < Sinatra::Base
 
     puts "suspects: #{subset.length}"
 
-    run_specs(subset + [victim])
+    specs_to_run = (subset + [victim]).map {|name| SpecFile.new(name) }
+    run_specs()
   end
 
   get "/" do
